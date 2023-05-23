2efef1182959670428fe05c6a987f6fe3c6ffa23
YahirAlbores221228 <programcion2023@gmail.com>
2023-05-23 14:55:20 -0600

    [UPDATE] se quito un mensaje comentado

--- src/Producto/application/Getproductoid.ts
+++ src/Producto/application/Getproductoid.ts
@@ -1,14 +1,4 @@
-/* import { ProductoRepository } from "../domain/Producto-repository";
 
-export class ProductoID{
-constructor(readonly productoRepository: ProductoRepository){}
-
-async run(id: number) {
-    const producto = await this.productoRepository.getById(id);
-    if (!producto) throw new Error("Usuario no encontrado");
-console.log(producto);
-}
-} */
 import { ProductoRepository } from "../domain/Producto-repository";
 
 
